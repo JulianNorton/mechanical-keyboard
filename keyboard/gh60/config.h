a0cb9540f7a6c5bcb10de3a491fde96b64bcf4a8
julian <julian@juliannorton.com>
2015-07-26 14:50:41 -0400

    reseting makefile to original on github

--- keyboard/gh60/config.h
+++ keyboard/gh60/config.h
@@ -23,9 +23,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    geekhack
-#define PRODUCT         GH60
-#define DESCRIPTION     t.m.k. keyboard firmware for GH60
+#define MANUFACTURER    julian
+#define PRODUCT         julian
+#define DESCRIPTION     firmware for the teensy
 
 /* key matrix size */
 #define MATRIX_ROWS 5
