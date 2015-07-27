d4a7ce3ade2d1c94ff16b749b2a7db138093b168
julian <julian@juliannorton.com>
2015-07-26 22:29:24 -0400

    flipped matrix colums, checked rows, recompiled

--- keyboard/gh60/.dep/obj_gh60_lufa_matrix.o.d
+++ keyboard/gh60/.dep/obj_gh60_lufa_matrix.o.d
@@ -1,38 +1,36 @@
-obj_gh60_lufa/matrix.o: matrix.c config.h \
- ../../tmk_core/common/action_layer.h ../../tmk_core/common/keyboard.h \
- ../../tmk_core/common/action.h ../../tmk_core/common/keycode.h \
- ../../tmk_core/common/action_code.h ../../tmk_core/common/action_macro.h \
- ../../tmk_core/common/progmem.h ../../tmk_core/common/print.h \
- ../../tmk_core/common/util.h ../../tmk_core/common/avr/xprintf.h \
- ../../tmk_core/common/debug.h ../../tmk_core/common/print.h \
- ../../tmk_core/common/util.h ../../tmk_core/common/matrix.h
+obj_gh60_lufa/matrix.o: matrix.c config.h ../../common/action_layer.h \
+ ../../common/keyboard.h ../../common/action.h ../../common/keycode.h \
+ ../../common/action_code.h ../../common/action_macro.h \
+ ../../common/progmem.h ../../common/print.h ../../common/util.h \
+ ../../common/avr/xprintf.h ../../common/debug.h ../../common/print.h \
+ ../../common/util.h ../../common/matrix.h
 
 config.h:
 
-../../tmk_core/common/action_layer.h:
+../../common/action_layer.h:
 
-../../tmk_core/common/keyboard.h:
+../../common/keyboard.h:
 
-../../tmk_core/common/action.h:
+../../common/action.h:
 
-../../tmk_core/common/keycode.h:
+../../common/keycode.h:
 
-../../tmk_core/common/action_code.h:
+../../common/action_code.h:
 
-../../tmk_core/common/action_macro.h:
+../../common/action_macro.h:
 
-../../tmk_core/common/progmem.h:
+../../common/progmem.h:
 
-../../tmk_core/common/print.h:
+../../common/print.h:
 
-../../tmk_core/common/util.h:
+../../common/util.h:
 
-../../tmk_core/common/avr/xprintf.h:
+../../common/avr/xprintf.h:
 
-../../tmk_core/common/debug.h:
+../../common/debug.h:
 
-../../tmk_core/common/print.h:
+../../common/print.h:
 
-../../tmk_core/common/util.h:
+../../common/util.h:
 
-../../tmk_core/common/matrix.h:
+../../common/matrix.h:
