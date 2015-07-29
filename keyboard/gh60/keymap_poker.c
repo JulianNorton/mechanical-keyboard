aa3e2c6a2390edb758e2e3d6a5e1adca83c13283
julian <julian@juliannorton.com>
2015-07-28 22:03:34 -0400

    Disabling fn layer, fixing KN column letters

--- keyboard/gh60/keymap_poker.c
+++ keyboard/gh60/keymap_poker.c
@@ -1,18 +1,20 @@
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    KEYMAP(ESC,  1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS, EQL, BSLS, VOLU, \
-           TAB,  Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC, RBRC, BSPC, VOLD,     \
-           FN1,  A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT, ENT, PGUP,           \
-           LSFT, Z,   X,   C,   V,   B,   N,   M,  COMM, DOT, SLSH, RSFT,  UP, PGDN,           \
-           LCTRL, LALT, LGUI,       SPC,       RGUI, RALT, RCTRL, LEFT, DOWN, RIGHT),
+    KEYMAP(ESC,  1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS, EQL, BSPC, GRV, \
+           TAB,  Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC, RBRC, BSLS, 2,     \
+           FN1,  A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT, ENT, 3,           \
+           LSFT, Z,   X,   C,   V,   B,   N,   M,  COMM, DOT, SLSH, RSFT,  UP, 4,           \
+           LCTL, LALT, LGUI,       SPC,       RGUI, RALT, RCTRL, LEFT, DOWN, RIGHT),
 
     /* 1: FN 1 */
-    KEYMAP(GRV,  F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,  TRNS, TRNS,     \
+    /*KEYMAP(GRV,  F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,  TRNS, TRNS,     \
            TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,     \
-           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, MUTE,           \
-           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, PGUP, TRNS,           \
-           TRNS, TRNS, TRNS,              SPC, TRNS, TRNS, TRNS, TRNS, PGDOWN, TRNS ),
+           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, 6,           \
+           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, 1, TRNS,           \
+           TRNS, TRNS, TRNS,              SPC, TRNS, TRNS, TRNS, TRNS, 2, TRNS ),*/
+
+
 };
 
 const uint16_t PROGMEM fn_actions[] = {
