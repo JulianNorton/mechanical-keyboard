55b00e73471c03724ce8e6445754cd91532372f8
julian <julian@juliannorton.com>
2015-07-28 22:14:07 -0400

    trying to mess with keymap files

--- keyboard/gh60/keymap_poker.c
+++ keyboard/gh60/keymap_poker.c
@@ -2,10 +2,11 @@
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(ESC,  1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS, EQL, BSPC, GRV, \
-           TAB,  Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC, RBRC, BSLS, 2,     \
-           FN1,  A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT, ENT, 3,           \
-           LSFT, Z,   X,   C,   V,   B,   N,   M,  COMM, DOT, SLSH, RSFT,  UP, 4,           \
+           TAB,  Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC, RBRC, BSLS, GRV,     \
+           GRV,  A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT, ENT, GRV,           \
+           LSFT, Z,   X,   C,   V,   B,   N,   M,  COMM, DOT, SLSH, RSFT,  UP, GRV,           \
            LCTL, LALT, LGUI,       SPC,       RGUI, RALT, RCTRL, LEFT, DOWN, RIGHT),
+
 
     /* 1: FN 1 */
     /*KEYMAP(GRV,  F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,  TRNS, TRNS,     \
