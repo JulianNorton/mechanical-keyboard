d70686c730108ec90fd6a63dcdfea1ebb4f7f55e
julian <julian@juliannorton.com>
2015-07-31 17:44:32 -0400

    messing with the function layers

--- keyboard/gh60/keymap_poker.c
+++ keyboard/gh60/keymap_poker.c
@@ -6,14 +6,14 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TAB,  Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, LBRC, RBRC,  BSLS,  VOLD,  \
     FN0,   A,   S,   D,   F,   G,   H,   J,   K,   L,    SCLN,  QUOT,  ENT,  PGUP,  \
     LSFT,   Z,   X,   C,   V,   B,   N,   M,  COMM, DOT, SLSH,  RSFT,   UP,  PGDN,  \
-    LCTRL, LALT, LGUI,       SPC,         RGUI,  RALT, RCTRL, LEFT, DOWN, RIGHT),
+    LCTRL, LALT, LGUI,       SPC,         RGUI,  RALT, FN0, LEFT, DOWN, RIGHT),
 
 
    /* 1: FN 1 */
   KEYMAP(
     GRV,   F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10, F11,   F12,  DEL,  DEL,  \
     TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS, TRNS,  TRNS,  TRNS,  \
-    TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  \
+    TRNS,  TRNS,  SLEP,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  \
     TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  TRNS,  PGUP,  TRNS,  \
     TRNS,  TRNS,  TRNS,       TRNS,       TRNS,  TRNS, TRNS, TRNS, PGDN, TRNS),
 
